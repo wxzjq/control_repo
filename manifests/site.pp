e0ab386ea9c613b044ef58d4227e49ff75f584d4
Janus Zhang <janus.zhang@gmail.com>
2018-12-29 23:06:49 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ node default {
 }
 node 'ip-10-0-3-104.ap-southeast-1.compute.internal' {
   include role::master_server
-  file { 'root/README':
+  file { '/root/README':
   ensure  => file,
   content => $fqdn,
   }
